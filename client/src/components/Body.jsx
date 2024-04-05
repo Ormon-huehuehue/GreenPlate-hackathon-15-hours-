efd7efb91b935c9b8e8ce66348893e41300e5e02
Arihant Sharma <101751599+yuuaki1@users.noreply.github.com>
2024-04-05 17:39:55 +0530

    Committed Body inside the components Folder

---
+++ client/src/components/Body.jsx
@@ -0,0 +1,9 @@
+function Body () {
+
+    return(<div className="giganiga">
+        <h1 id = "a1">The only ERP you will ever need</h1>
+        <p id = "a2">Our services and our pricing will have you coming back to us again and again</p>
+    </div>)
+}
+
+export default Body
