3224a13cdb824d7176a90f98a169378f5cf30bfd
Arihant Sharma <101751599+yuuaki1@users.noreply.github.com>
2024-04-05 17:48:30 +0530

    trying out git commits

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,10 +3,14 @@ import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <nav>
-        <Link to ='/'>Home</Link>
-        <Link to = '/register'>Register</Link>
-        <Link to = '/login'>Login</Link>
+    <nav className='navbar'>
+        <h1>ERP</h1>
+        <div className='links'>
+            <Link to='/'>Home</Link>
+            <Link to='/register'>Register</Link>
+            <Link to='/login'>Login</Link>
+            <Link to='/'
+        </div>
     </nav>
   )
 }
