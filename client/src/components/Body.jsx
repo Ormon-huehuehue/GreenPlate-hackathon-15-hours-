ddd7aeeba6d2122e8056cb9cf93f87a544946024
Ormon-huehuehue <armnvrma10@gmail.com>
2024-04-06 07:21:34 +0530

    removed faaltu folder

--- client/src/components/Body.jsx
+++
@@ -1,9 +0,0 @@
-function Body () {
-
-    return(<div className="body-text">
-        <h1 id = "a1">The only ERP you will ever need</h1>
-        <p id = "a2">Our services and our pricing will have you coming back to us again and again</p>
-    </div>)
-}
-
-export default Body
--- client/src/pages/Contact us.jsx
+++
@@ -1,5 +0,0 @@
-export default function Contact() {
-  return (
-    <div>Contact Us</div>
-  )
-}
